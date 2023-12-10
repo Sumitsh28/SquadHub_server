3253aad8168c263c92bf9f8cd076ecf8fd853788
Sumitsh28 <sumit.off28@gmail.com>
2023-12-10 21:20:09 +0530

    Init

---
+++ src/services/jwt.ts
@@ -0,0 +1,18 @@
+import JWT from "jsonwebtoken";
+import { User } from "next-auth";
+
+const JWT_SECRET = "Super@1234";
+
+class JWTService {
+  public static generateTokenForUser(user: User) {
+    const payload = {
+      id: user?.id,
+      email: user?.email,
+    };
+
+    const token = JWT.sign(payload, JWT_SECRET);
+    return token;
+  }
+}
+
+export default JWTService;
